78b3799484b8fd7bdd5b7d5de26c061b551745d6
IgnacioDonosoS <ignacio.donoso.s@gmail.com>
2020-06-10 15:55:24 -0400

    intento 1

--- Tutorial/src/tutorial/Tutorial.java
+++ Tutorial/src/tutorial/Tutorial.java
@@ -1,11 +1,25 @@
 
 package tutorial;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Tutorial {
 
-    public static void main(String[] args) {
-        System.out.println("pico");
-    }
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("nombres.txt"));
+        ArrayList<String> estudiantes = new ArrayList<>();
+        
+        while (sc.hasNextLine()) {
+        estudiantes.add(sc.nextLine());
+        }
+        
+        for (int i = 0; i < estudiantes.size(); i++) {
+            System.out.println(estudiantes.get(i)); 
+        }
+            
+        }
     
 }
