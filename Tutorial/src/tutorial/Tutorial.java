a94d384ec3db71696b74f33785b66864cc86ced7
IgnacioDonosoS <ignacio.donoso.s@gmail.com>
2020-06-11 14:14:27 -0400

    arrays 2 dimensiones

--- Tutorial/src/tutorial/Tutorial.java
+++ Tutorial/src/tutorial/Tutorial.java
@@ -9,35 +9,21 @@ import java.util.LinkedList;
 
 public class Tutorial {
 
-
     public static void main(String[] args) throws FileNotFoundException {
-            int o = 0;
-
-        LinkedList<String> people = new LinkedList<>();
-        people.add("Perro");
-        people.push("casa");
-        people.add(0, "porton");
-
-//        Iterator<String> iter = people.iterator();
-//        {
-//            while (iter.hasNext()) {
-//                System.out.println(iter.next());
-//                
-//            }
-//        }
-//        for (int i = 0; i < people.size(); i++) {
-//            String get = people.get(i);
-//            System.out.println(people.get(i));
-//        }
-//        
-//        for (Iterator<String> iterator = people.iterator(); iterator.hasNext();) {
-//            System.out.println(iterator.next());
-//            
-//        }
-        for (String string : people) {
-            System.out.println(people.get(o++));
+       
+        int[][] data =  {
+         {1, 1, 1, 0, 0 },
+         {1, 0, 0, 0, 1 },
+         {1, 2, 1, 1, 1 },
+        };
+        
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[0].length; j++) {
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println("");
             
         }
+    
     }
-
 }
