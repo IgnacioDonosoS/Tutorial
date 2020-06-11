820a7639c274e0822835f77fdf807a23515f6960
IgnacioDonosoS <ignacio.donoso.s@gmail.com>
2020-06-11 11:12:44 -0400

    stacks

--- Tutorial/src/tutorial/Tutorial.java
+++ Tutorial/src/tutorial/Tutorial.java
@@ -6,24 +6,22 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Tutorial {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner(new File("nombres.txt"));
-        ArrayList<String> estudiantes = new ArrayList<>();
+      
+        LinkedList<String> people = new LinkedList<>();
+        people.add("Perro");
+        people.push("casa");
+        people.add(0, "porton");
         
-        while (sc.hasNextLine()) {
-        estudiantes.add(sc.nextLine());
-        }
+        System.out.println(people.remove());
+        System.out.println(people.remove());
+        System.out.println(people.remove());
         
-        for (int i = 0; i < estudiantes.size(); i++) {
-            System.out.println(estudiantes.get(i)); 
-        }
-        Iterator<String> iter = estudiantes.iterator();
-        while (iter.hasNext()) {            
-            System.out.println(iter.next());
-        }
+                
         }
     
 }
